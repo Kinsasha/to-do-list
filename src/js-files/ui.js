242cce2cb108f82134342869697ba1b596a5e20e
Kinsasha <kennedyokechukwu203@gmail.com>
2026-01-13 20:30:20 +0100

    set up webpack

---
+++ src/js-files/ui.js
@@ -0,0 +1,5 @@
+const renderDefaultDOM = () => {
+  const content = document.querySelector(".content");
+  const sidebar = document.querySelector(".sideBar");
+  const header = document.querySelector(".heroText");
+};
